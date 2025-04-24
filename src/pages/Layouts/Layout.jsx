b968637774510ef844b728536798bb9a8d1aea55
Abdulbasit Ahmadzai <basitahmadzai2025@gmail.com>
2025-04-24 16:16:17 +0430

    Update Layout.jsx

--- src/pages/Layouts/Layout.jsx
+++ src/pages/Layouts/Layout.jsx
@@ -119,7 +119,7 @@ function Layout() {
           </div>
         </div>
         <div className="hero-img">
-          <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="logo" />
+          <img src="/src/assets/images/BasitOwnd.PNG" alt="logo" />
         </div>
       </div>
       {/* percentage section  */}
